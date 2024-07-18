b1d202f3acb5062a917db7e75c513061a0a0c782
Christopher Gholmieh <149006465+NapoleanC@users.noreply.github.com>
2024-07-18 14:53:44 -0700

    Purging packages

--- scripts/malicious-packages.sh
+++ scripts/malicious-packages.sh
@@ -11,7 +11,7 @@ function main() {
     for malicious_package in "${mallicious_packages[@]}"; do
         if [[ $(is_package_installed "${malicious_package}") == "1" ]]; then
             if [[ $(prompt "Would you like to remove ${malicious_package}? [y/N]: ") == "1" ]]; then
-                apt remove "${malicious_package}"
+                apt purge "${malicious_package}"
             fi
         fi
     done
